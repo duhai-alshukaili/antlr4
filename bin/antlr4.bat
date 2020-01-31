3ac37d9d7f1cba5d466c3556600825aa5819dca0
Duhai Alshukaili <duhai.alshukaili@gmail.com>
2020-02-01 00:40:57 +0400

    Updated antlr4 to write logs

--- bin/antlr4.bat
+++ bin/antlr4.bat
@@ -1,4 +1,4 @@
 @echo off
 
 REM Run ANTLR4 Parser Generator
-java -classpath %ANTLR4_HOME%\lib\antlr-4.7.2-complete.jar;. org.antlr.v4.Tool %*
+java -classpath %ANTLR4_HOME%\lib\antlr-4.7.2-complete.jar;. org.antlr.v4.Tool -Xlog %*
